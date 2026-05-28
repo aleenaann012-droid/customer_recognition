5006b1b77e5ba40de8b3a0f3942033b4e06e5d7d
aleenaannaji <aleenaann012@gmail.com>
2026-05-28 12:27:41 +0530

    staff_recognition on process

--- src/database.py
+++ src/database.py
@@ -6,22 +6,48 @@ cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS customers(
-    customer_id INTEGER PRIMARY KEY,
+    customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
     age INTEGER,
     gender TEXT,
+    
+    visit_count INTEGER DEFAULT 1,
+    
     entry_time TEXT,
     exit_time TEXT,
-    visit_count INTEGER,
+    
+    visit_date TEXT,
+    last_seen TEXT,
+    
     embedding BLOB
 )
 """)
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS staff(
-    staff_id INTEGER PRIMARY KEY,
+    staff_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
     name TEXT,
     role TEXT,
+    
     embedding BLOB
 )
 """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS staff_attendance(
+    attendance_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
+    staff_id INTEGER,
+    
+    entry_time TEXT,
+    exit_time TEXT,
+    
+    date TEXT,
+    
+    FOREIGN KEY(staff_id) REFERENCES staff(staff_id)
+)
+""")
+
 conn.commit()
 conn.close()
